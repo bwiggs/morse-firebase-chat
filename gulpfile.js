b499b9e4d830d75fc889aa0e96cdcc86b94ed9e1
Brian Wigginton <brianwigginton@gmail.com>
2016-08-16 16:40:15 -0500

    adding feature files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-var APP_JS = 'app/app.js';
+var APP_JS = 'app/**/*.js';
+var APP_CSS = 'app/**/*.css';
 
-var VENDOR_CSS = [
-  'node_modules/materialize-css/dist/css/materialize.min.css'
-];
+var VENDOR_CSS = [];
 
 var VENDOR_JS = [
-  'node_modules/jquery/dist/jquery.min.js',
-  'node_modules/materialize-css/dist/js/materialize.min.js',
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/handlebars/dist/handlebars.js',
+  'node_modules/underscore/underscore.js',
+  'node_modules/backbone/backbone.js',
+  'node_modules/moment/min/moment.min.js',
   'node_modules/firebase/firebase-app.js',
   'node_modules/firebase/firebase-database.js'
 ];
@@ -25,6 +27,10 @@ gulp.task('build', function() {
   gulp.src(APP_JS)
       .pipe(concat('app.js'))
       .pipe(gulp.dest('public/js'));
+
+  gulp.src(APP_CSS)
+      .pipe(concat('app.css'))
+      .pipe(gulp.dest('public/css'));
 
   gulp.src(VENDOR_CSS)
       .pipe(concat('vendor.css'))
